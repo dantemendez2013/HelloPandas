72d0fc6c1c5c4f8a73aff2a79c05e42339fb8a4e
dantemendez2013 <24309167+dantemendez2013@users.noreply.github.com>
2025-04-05 22:45:44 -0500

    Integrated Parquet

--- python/HelloPandas/HelloPandas/HelloPandasRunner.py
+++ python/HelloPandas/HelloPandas/HelloPandasRunner.py
@@ -3,8 +3,10 @@ import pandas as pd
 
 @click.command()
 @click.argument('csv_file_path', type=click.Path(exists=True))
-def cli(csv_file_path: str):
-    print(f'Hello Pandas:\n{pd.read_csv(csv_file_path)}')
+@click.argument('parquet_file_path', type=click.Path(exists=True))
+def cli(csv_file_path: str, parquet_file_path: str):
+    print(f'Hello Pandas CSV:\n{pd.read_csv(csv_file_path)}')
+    print(f'Hello Pandas Parquet:\n{pd.read_parquet(parquet_file_path)}')
 
 if __name__ == '__main__':
     cli()
